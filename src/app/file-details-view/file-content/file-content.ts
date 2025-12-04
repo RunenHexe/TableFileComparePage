fd60f373bbabad4df700a228f7d2cb9353409a56
RunenHexe <RigggedRuneWitch@gmail.com>
2025-12-04 22:41:49 +0100

    added file column view and selection

--- src/app/file-details-view/file-content/file-content.ts
+++ src/app/file-details-view/file-content/file-content.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges} from '@angular/core';
+import {FileData} from '../../interfaces/file-data';
 
 @Component({
   selector: 'app-file-content',
@@ -7,6 +8,62 @@ import { Component } from '@angular/core';
   styleUrl: './file-content.css',
   standalone: true
 })
-export class FileContent {
+export class FileContent implements OnChanges, OnInit{
+  @Input() file: FileData | null = null;
 
+  @Input() fileSeparator: string = ";"
+
+  @Input() hasColumnNaming: boolean = false;
+
+  @Output() selectedLines = new EventEmitter<String[]>();
+
+  fileContentLines: string[] = []
+  fileContentColumns: string[][] = []
+
+  maxColumns = 0;
+
+  longestColumn: string[] = []
+
+  activeColumn: number = 0
+
+  selectedColumnContent: string[] = []
+
+  localFileContent: String = ''
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if(changes['file'] && this.file != null){
+      this.localFileContent = this.file.content
+      this.fileContentLines = this.localFileContent.split("\r\n")
+      for(let i = 0; i < this.fileContentLines.length; i++){
+        this.fileContentColumns[i] = this.fileContentLines[i].split(this.fileSeparator)
+        if(this.maxColumns < this.fileContentColumns[i].length) {
+          this.maxColumns = this.fileContentColumns[i].length
+          this.longestColumn = this.fileContentColumns[i]
+        }
+      }
+    }
+  }
+
+  ngOnInit(): void {
+    if(this.file != null){
+      this.localFileContent = this.file.content
+      this.fileContentLines = this.localFileContent.split("\r\n")
+      for(let i = 0; i < this.fileContentLines.length; i++){
+        this.fileContentColumns[i] = this.fileContentLines[i].split(this.fileSeparator)
+      }
+    }
+  }
+
+  changeActiveColumn($index: number) {
+    this.activeColumn = $index;
+    this.selectedColumnContent = []
+    if(this.fileContentLines != null){
+      for(let i = 0; i < this.fileContentLines.length; i++){
+        if(this.fileContentColumns[i].length > 0) {
+          this.selectedColumnContent.push(this.fileContentColumns[i][this.activeColumn])
+        }
+      }
+    }
+    this.selectedLines.emit(this.selectedColumnContent)
+  }
 }
